0605b505b8e1d61299f7aea53689c5c4716776b0
AsiF-Py <asif.mannanuddin@gmail.com>
2025-04-23 16:26:02 +0600

    changes in ios file

--- ios/PinTrackerTests/PinTrackerTests.m
+++ ios/PinTrackerTests/PinTrackerTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DistenyPinTests : XCTestCase
+@interface PinTrackerTests : XCTestCase
 
 @end
 
-@implementation DistenyPinTests
+@implementation PinTrackerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
